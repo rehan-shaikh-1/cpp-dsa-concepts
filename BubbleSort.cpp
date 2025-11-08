e3c614e0cfc9e2212ac98d9bd1397aad360a0a08
REHAN SHAIKH <rehan.rajebhai.shaikh@gmail.com>
2025-11-08 11:58:38 +0530

    Fix formatting issues in BubbleSort function

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -18,9 +18,9 @@ void BubbleSort(vector<int> &arr) {
     int n = arr.size();
     bool swapped;
 
-    for (int i = 0; i < n - 1;i++) {
+    for (int i = 0; i < n - 1; i++) {
         swapped = false;
-        for (int j = 0; j < n - i - 1;j++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
